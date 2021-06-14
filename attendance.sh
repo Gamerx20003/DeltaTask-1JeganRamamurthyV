514bfebcbca094821ed21d01d4635f0af639605f
Jegan Ramamurthy V <jeganramamurthyv@gmail.com>
2021-06-14 22:59:16 +0530

    attendance.sh logical error rectified

--- attendance.sh
+++ attendance.sh
@@ -1,38 +1,48 @@
-#! /bin/bash
+#!/bin/bash
 
-cd /home/jay_jay
+declare -A userdate        
+declare -a all
 
+for user in {webdev_,sysad_,appdev_}{1..30}
+do
+    all+=("$user")
+done
+
+date1="2019-09-27"
+date2=$(date +%F)
 echo Enter Date 1 and Date 2 in yyyy-mm-dd format:
 read date1 date2
 
-if [ $# -eq 0 ]
-then
-  while IFS=" ""," read c1 c2 c3 c4
-  do 
-    finish=$(date +%s)
-    start=$(date +%s -d '2019-09-27')
-    now=$(c3 +%s)
-    if [ $now -ge $start ] && [ $now -le $finish ]
-      then
-         continue
-      else
-         echo "$c1 was absent on $c3" >> attendance.txt
+
+date2=$(date -d "$date2 1 days" +'%Y-%m-%d')
+
+while read line
+do
+    name=$(cut -d' ' -f 1 <<<$line) 
+    date=$(cut -d' ' -f 3 <<<$line) 
+    userdate[$date]="${userdate[$date]} $name"
+done < /home/jegan/Desktop/attendance.log
+
+while [ "$date1" != "$date2" ]
+do
+    dwc="$date1,"
+    if [ "${userdate[$dwc]}" != "" ]     
+    then
+        declare -A dummy   
+        for variable in ${userdate[$dwc]}     
+        do
+            dummy[$variable]="present"
+        done
+        echo "Absent on $date1 are..."    
+        for people in ${all[*]}
+        do
+            if [ "${dummy[$people]}" != "present" ]
+            then
+                echo $people
+            fi 
+        done
+        unset dummy
+        echo
     fi
-  done < /home/jegan/Desktop/attendance.log
-else
-  start=$(date +%s -d '$date1')
-  finish=$(date +%s -d '$date2')
-
-  while IFS=" ""," read g1 g2 g3 g4
-   do
-    now=$(date +%s -d "$g3")
-      if [ $now -ge $start ] && [ $now -le $finish ]
-        then
-           continue
-      else
-           echo "$g1 was absent on $g3" >> attendance.txt
-      fi
-   done < /home/jegan/Desktop/attendance.log   
-fi  
-
-
+    date1=$(date -d "$date1 1 days" +'%Y-%m-%d')
+done
