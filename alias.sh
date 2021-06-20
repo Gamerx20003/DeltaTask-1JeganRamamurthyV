9e152fd4e858e2317d9a6d5ff4375c1cd0561e70
Jegan Ramamurthy V <jeganramamurthyv@gmail.com>
2021-06-20 20:41:52 +0530

    super user done

--- alias.sh
+++ alias.sh
@@ -5,3 +5,4 @@ echo alias permit="/home/jegan/Desktop/permit.sh" >> .bashrc
 echo alias attendance="/home/jegan/Desktop/attendance.sh" >> .bashrc
 echo alias schedule="/home/jegan/Desktop/schedule.sh" >> .bashrc
 echo alias getMOM="/home/jegan/Desktop/getMOM.sh" >> .bashrc
+echo alias genMOM="/home/jegan/Desktop/genMOM.sh" >> .bashrc
